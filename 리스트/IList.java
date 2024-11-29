3b458b3f17e20885d28b7070e83c587295e15ba4
kimsy98 <119915194+kimsy98@users.noreply.github.com>
2024-11-29 13:53:19 +0900

    Create IList.java

---
+++ 리스트/IList.java
@@ -0,0 +1,24 @@
+package 자료구조구현;
+
+public interface IList<T> {
+
+    void add(T t);
+
+    void insert(int index, T t);
+
+    void clear();
+
+    boolean delete(T t);
+
+    boolean deleteByIndex(int index);
+
+    T get(int index);
+
+    int indexOf(T t);
+
+    boolean isEmpty();
+
+    boolean contains(T t);
+
+    int size();
+}
